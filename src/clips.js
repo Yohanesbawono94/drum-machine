9fc626ae8af2ca275764e6094d434217f1187775
Yohanesbawono94 <hanes.aness@gmail.com>
2018-09-09 11:01:01 +0700

    add object data in clips.js

---
+++ src/clips.js
@@ -0,0 +1,113 @@
+export const clipsOne = [
+    {
+      id: 'tabla-dum',
+      keyCode: 81,
+      keyTrigger: 'Q',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1534589813/drum%20machine/clip1/tabla-dum.wav'
+    },
+    {
+      id: 'lo-tom',
+      keyCode: 87,
+      keyTrigger: 'W',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1534589810/drum%20machine/clip1/lotom.wav'
+    },
+    {
+      id: 'juicy-snare',
+      keyCode: 69,
+      keyTrigger: 'E',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1535017593/drum%20machine/clip1/juicy_snare.wav'
+    },
+    {
+      id: 'berimbau',
+      keyCode: 65,
+      keyTrigger: 'A',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1534589805/drum%20machine/clip1/berimbau.wav'
+    },
+    {
+      id: 'dum-tom',
+      keyCode: 83,
+      keyTrigger: 'S',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1535017591/drum%20machine/clip1/aphexbd_-_dum-tom.wav'
+    },
+    {
+      id: 'drum-up',
+      keyCode: 68,
+      keyTrigger: 'D',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1535017590/drum%20machine/clip1/2ghostsds_-_drum.wav'
+    },
+    {
+      id: 'snare',
+      keyCode: 90,
+      keyTrigger: 'Z',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1535017591/drum%20machine/clip1/707-ohh_-_snare.wav'
+    },
+    {
+      id: 'bass',
+      keyCode: 88,
+      keyTrigger: 'X',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1535017591/drum%20machine/clip1/808bd2_-_bass.wav'
+    },
+    {
+      id: 'clap',
+      keyCode: 67,
+      keyTrigger: 'C',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1534589803/drum%20machine/clip1/clap-1.wav'
+    }
+  ];
+  
+  export const clipsTwo = [
+    {
+      id: 'bass-punch',
+      keyCode: 81,
+      keyTrigger: 'Q',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1534589815/drum%20machine/clip%202/bass-rhodes-punch.wav'
+    },
+    {
+      id: 'thin-clap',
+      keyCode: 87,
+      keyTrigger: 'W',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1534589813/drum%20machine/clip%202/thin-wispy-clap.wav'
+    },
+    {
+      id: 'resy-bass',
+      keyCode: 69,
+      keyTrigger: 'E',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1535019258/drum%20machine/clip%202/resybass.wav'
+    },
+    {
+      id: 'guitar-1',
+      keyCode: 65,
+      keyTrigger: 'A',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1534589805/drum%20machine/clip%202/disco-guitar-notes-1.wav'
+    },
+    {
+      id: 'guitar-2',
+      keyCode: 83,
+      keyTrigger: 'S',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1534589809/drum%20machine/clip%202/disco-guitar-notes-2.wav' 
+    },
+    {
+      id: 'guitar-3',
+      keyCode: 68,
+      keyTrigger: 'D',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1534589808/drum%20machine/clip%202/disco-guitar-notes-3.wav' 
+    },
+    {
+      id: 'flarz-bass',
+      keyCode: 90,
+      keyTrigger: 'Z',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1534589809/drum%20machine/clip%202/flarzbass.wav'
+    },
+    {
+      id: 'alarmblop',
+      keyCode: 88,
+      keyTrigger: 'X',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1534589806/drum%20machine/clip%202/alarmblop.wav'
+    },
+    {
+      id: 'jazz-hit',
+      keyCode: 67,
+      keyTrigger: 'C',
+      src: 'https://res.cloudinary.com/dhdmx0ldw/video/upload/v1534589808/drum%20machine/clip%202/jazz_hit.wav'
+    }
+  ]
