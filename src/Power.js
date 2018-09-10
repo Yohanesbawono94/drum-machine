ce5407deaa77f7f2e07673314a7c14f201a1ae77
Yohanesbawono94 <hanes.aness@gmail.com>
2018-09-10 23:46:47 +0700

    delete constructor function

--- src/Power.js
+++ src/Power.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import '/App.css';
 
 class Power extends Component {
-    constructor(props) {
-      super(props);
-    }
     render () {
       return (
         <div className='power-container'>
--- src/SoundsToggle.js
+++ src/SoundsToggle.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 class SoundsToggle extends Component {
-    constructor(props) {
-     super(props);
-   }
    render () {
      return (
        <div className='soundstoggle-container'>
