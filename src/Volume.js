dbc5ee3aaff3764f13f36ab41dd26d8cb81c0a20
Yohanesbawono94 <hanes.aness@gmail.com>
2018-09-10 23:44:59 +0700

    add Volume component

---
+++ src/Volume.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Volume extends Component {
+   render () {
+     return (
+       <div className='volume-container'>
+         <input 
+           type='range'
+           min='0'
+           max='1'
+           step='0.01'
+           value={this.props.slider}
+           onChange={this.props.adjustVol}/>
+       </div>
+     )
+   }
+ }   
+
+ export default Volume;
