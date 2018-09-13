2423837a103e000b9ace512734a858347c482791
Yohanesbawono94 <hanes.aness@gmail.com>
2018-09-13 11:48:26 +0700

    modify App.css source

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './css/App.css';
 import { clipsOne, clipsTwo } from './clips.js';
 
 class App extends Component {
--- src/Drumpads.js
+++ src/Drumpads.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './css/App.css';
 
 class DrumPad extends Component {
     constructor(props) {
